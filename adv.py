7ea26097e65f20c94c084f28ced172adb331f234
amindazad <amindazad@icloud.com>
2020-08-13 23:32:21 -0700

    Added BFS Solution

--- adv.py
+++ adv.py
@@ -5,27 +5,25 @@ from world import World
 import random
 from ast import literal_eval
 
-class Stack():
+class Queue():
     def __init__(self):
-        self.stack = []
+        self.queue = []
 
-    def __str__(self):
-        return str(self.stack)
+    def enqueue(self, value):
+        self.queue.append(value)
 
-    def push(self, value):
-        self.stack.append(value)
-    def pop(self):
+    def dequeue(self):
         if self.size() > 0:
-            return self.stack.pop()
+            return self.queue.pop(0)
         else:
             return None
+
     def size(self):
-        return len(self.stack)
+        return len(self.queue)
 
 class Graph:
     def __init__(self):
         self.visited = {}
-        self.reverse_path = Stack()
         self.reverse_directions = {
             'n': 's',
             'w': 'e',
@@ -53,19 +51,12 @@ class Graph:
 
 
     def dft(self, direction=None, previous_room=None):
-        # If we've visited all rooms, stop recursing
-        if len(self.visited) == len(room_graph):
-            return
-
         # If a direction was passed in
         if direction is not None:
             # Travel in that direction
             player.travel(direction)
             # Add the direction to the path
             traversal_path.append(direction)
-
-            # Add the reverse direction to the reverse path
-            self.reverse_path.push(self.reverse_directions[direction])
 
         # Get the current room
         current_room = player.current_room
@@ -93,27 +84,85 @@ class Graph:
                                self.reverse_directions[direction],
                                previous_room.id)
 
+        # If we've visited all rooms, stop recursing
+        if len(self.visited) == len(room_graph):
+            return
+
         # Get the unvisited exits of the current room
         unvisited_exits = self.get_unvisited_exits(current_room.id)
 
         # If there are unvisited exits
         if len(unvisited_exits) > 0:
-            # Recurse through the unvisited exits
-            for exit in unvisited_exits:
-                self.dft(exit, current_room)
 
-        # When there are no unvisited exits
-        # Pop off the latest the reverse direction
-        reverse_step = self.reverse_path.pop()
+           if len(unvisited_exits) == 1:
+                self.dft(unvisited_exits[0], current_room)
+           else:
+                # Recurse through a random unvisited exit
+                random_int = random.randint(0, len(unvisited_exits) - 1)
+                self.dft(unvisited_exits[random_int], current_room)
 
-        # Move in that direction
-        player.travel(reverse_step)
+        else:
+            path_to_nearest_unvisited = self.bfs(current_room)
 
-        # Append the step to the path
-        traversal_path.append(reverse_step)
+            if path_to_nearest_unvisited is None:
+               return
+
+            # Get the destination room
+            destination_room = path_to_nearest_unvisited[-1][0]
+
+            # Get just the directions, excluding room ids
+            shortest_path = list(map(lambda x: x[1], path_to_nearest_unvisited))
+
+            # Add the shortest_path to the traversal_path
+            traversal_path.extend(shortest_path)
+
+            # Move the player there
+            for step in shortest_path:
+                player.travel(step)
+
+            # Perform another dft starting at the destination room
+            self.dft()
+
+
+    def bfs(self, starting_room):
+        visited = set()
+
+        # Initialize a path with the starting room
+        path = [(starting_room.id, None)]
+
+        # Set up a queue for enqueuing paths
+        q = Queue()
+
+        # Enqueue the path
+        q.enqueue(path)
+
+        # Iterate while the queue is not empty
+        while q.size() > 0:
+            # Dequeue the current path
+            current_path = q.dequeue()
+
+            # Get the current room, aka the last room in the path
+            current_room = current_path[-1][0]
+
+            # Get the unvisited exits of the current room
+            unvisited_exits = self.get_unvisited_exits(current_room)
+
+            # If there are unvisited exits
+            if len(unvisited_exits) > 0:
+                # Return the path, leaving out the starting room
+                return current_path[1:]
+
+            # Iterate over the current room's exits
+            for direction, room_id in self.visited[current_room].items():
+                # Enqueue a new path for each exit
+                q.enqueue(current_path + [(room_id, direction)])
+
+        return None
+
 
 # Load world
 world = World()
+
 
 # You may uncomment the smaller graphs for development and testing purposes.
 # map_file = "maps/test_line.txt"
@@ -121,12 +170,16 @@ world = World()
 # map_file = "maps/test_loop.txt"
 # map_file = "maps/test_loop_fork.txt"
 map_file = "maps/main_maze.txt"
+
 # Loads the map into a dictionary
 room_graph=literal_eval(open(map_file, "r").read())
 world.load_graph(room_graph)
+
 # Print an ASCII map
 world.print_rooms()
+
 player = Player(world.starting_room)
+
 # Fill this out with directions to walk
 # traversal_path = ['n', 'n']
 traversal_path = []
@@ -134,7 +187,35 @@ traversal_path = []
 world_graph = Graph()
 world_graph.dft()
 
-#print(world_graph.visited)
+print(traversal_path)
+print(world_graph.visited)
 
 # TRAVERSAL TEST - DO NOT MODIFY
 visited_rooms = set()
+player.current_room = world.starting_room
+visited_rooms.add(player.current_room)
+
+for move in traversal_path:
+    player.travel(move)
+    visited_rooms.add(player.current_room)
+
+if len(visited_rooms) == len(room_graph):
+    print(f"TESTS PASSED: {len(traversal_path)} moves, {len(visited_rooms)} rooms visited")
+else:
+    print("TESTS FAILED: INCOMPLETE TRAVERSAL")
+    print(f"{len(room_graph) - len(visited_rooms)} unvisited rooms")
+
+
+
+#######
+# UNCOMMENT TO WALK AROUND
+#######
+player.current_room.print_room_description(player)
+while True:
+    cmds = input("-> ").lower().split(" ")
+    if cmds[0] in ["n", "s", "e", "w"]:
+        player.travel(cmds[0], True)
+    elif cmds[0] == "q":
+        break
+    else:
+        print("I did not understand that command.")
